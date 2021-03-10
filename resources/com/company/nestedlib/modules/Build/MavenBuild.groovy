3036e8965aba855b4f342d769aabadeedd78b5c4
Maxime Michel <maxime.michel@magnolia-cms.com>
2021-03-10 13:01:10 +0100

    Do not use sed

--- resources/com/company/nestedlib/modules/Build/MavenBuild.groovy
+++ resources/com/company/nestedlib/modules/Build/MavenBuild.groovy
@@ -5,7 +5,7 @@
 echo "It's overridden MavenBuild started - let's run the original one"
 
 sh """echo "Let's do ${CFG.'maven.additional_option' ?: 'something important'} here before the maven build" > testfile.txt"""
-sh """sed -i 's/1.0.0-SNAPSHOT/1.2.0-SNAPSHOT/' pom.xml"""
+// sh """sed -i 's/1.0.0-SNAPSHOT/1.2.0-SNAPSHOT/' pom.xml"""
 
 MPLModule('Maven Build', CFG)
 
